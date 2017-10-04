666e9a24f48dc5b9d723a9d65e0b0da07890b881
skyrepprac <viveks@e2msol.com>
2017-10-04 12:31:05 +0530

    Create ExtCleanPlayground.php

---
+++ PHP/ExtCleanPlayground.php
@@ -0,0 +1,23 @@
+<?php
+class QuickTest extends \PHPUnit_Framework_TestCase
+{
+	public function testExample()
+	{
+		//instantiate your class
+		$context = new Magento\Framework\Object();
+
+		$context->setData('param', 'value');
+
+		//test whatever you want to test
+		$this->assertEquals('value', $context->getData('param'));
+
+		//you could even output to console
+		echo $context->getData('param');
+	}
+}
+
+/*
+then from the directory dev/tests/unit/ run phpunit quicktest.php which will execute your code. This all works since the file dev/tests/unit/phpunit.xml.dist is automatically loaded and prepares the environment.
+
+In a lot of cases you might have to supply input to the classes' constructor. Please see the existing tests under dev/tests/unit/testsuite/ for further examples of how this could look, including mocking objects.
+*/
